56f6336579a344f6e59245936e7d6bb27a956abe
Raiyan109 <kraiyan109@gmail.com>
2024-06-06 22:37:46 +0600

    cart done

--- src/pages/products/Product.jsx
+++ src/pages/products/Product.jsx
@@ -7,23 +7,22 @@ import { AuthContext } from "../../context/AuthContext";
 const Product = ({ product }) => {
     const { mernAuth } = useContext(AuthContext)
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    const handleSubmit = async (id) => {
+        console.log(id);
         try {
             const res = await axios.post('http://localhost:5000/api/cart/addToCart', {
-                productId: product.productId,
-                quantity: product.quantity,
-                name: product.name,
-                price: product.price
+                productId: id,
+                quantity: 1,
             }, {
                 headers: {
-                    'Content-Type': 'multipart/form-data',
+                    'Content-Type': 'application/json',
                     "Authorization": `Bearer ${mernAuth?.token}`
                 }
             })
+            console.log(res.data); // Log the entire response data to inspect it
+            const data = res.data;
+            console.log("Cart updated:", data);
 
-            const data = await res.data.user
-            console.log(res);
             return data
         } catch (error) {
             console.error("Error adding to cart", error);
@@ -34,7 +33,7 @@ const Product = ({ product }) => {
             <img src={product.image} alt="" className="w-56 h-56 object-contain" />
             <h1>{product.name}</h1>
             <h1>{product.price}</h1>
-            <button className="bg-primary rounded-md py-2 px-5 w-28 text-sm" onClick={handleSubmit}>Add to cart</button>
+            <button className="bg-primary rounded-md py-2 px-5 w-28 text-sm" onClick={() => handleSubmit(product._id)}>Add to cart</button>
         </div>
     );
 };
