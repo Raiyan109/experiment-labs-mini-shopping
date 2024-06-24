bfe9fbace0ae0efb3c7f8c2317150e9a07e4bdde
Raiyan109 <kraiyan109@gmail.com>
2024-06-24 17:58:55 +0600

    ok1

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -15,18 +15,18 @@ export const CartContextProvider = ({ children }) => {
         (async () => {
             try {
                 setIsLoading(true)
-                // const res = await axios.get('https://experiment-labs-mini-shopping-be.vercel.app/api/cart', {
-                //     headers: {
-                //         'Content-Type': 'application/json',
-                //         "Authorization": `Bearer ${mernAuth?.token}`
-                //     }
-                // })
-                const res = await axios.get('http://localhost:5000/api/cart', {
+                const res = await axios.get('https://experiment-labs-mini-shopping-be.vercel.app/api/cart', {
                     headers: {
                         'Content-Type': 'application/json',
                         "Authorization": `Bearer ${mernAuth?.token}`
                     }
                 })
+                // const res = await axios.get('http://localhost:5000/api/cart', {
+                //     headers: {
+                //         'Content-Type': 'application/json',
+                //         "Authorization": `Bearer ${mernAuth?.token}`
+                //     }
+                // })
                 console.log(res.data);
                 if (res.data) {
                     setCartTotal(res.data.total);
